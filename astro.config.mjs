1658f8dfee149c5d3ab3bc52dd17a74c88647fe9
Jan Kohlbach <mail@jankohlbach.dev>
2024-11-26 16:17:12 +0100

    ✨ add few more comments and base settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,10 +6,18 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  // TODO: change to final domain
   site: 'https://astro-creative-base.vercel.app',
   integrations: [
     robotsTxt({
       sitemapBaseFileName: 'sitemap-index',
+      // TODO: remove when going live
+      // policy: [
+      //   {
+      //     userAgent: '*',
+      //     disallow: '/',
+      //   },
+      // ],
     }),
     sitemap(),
   ],
